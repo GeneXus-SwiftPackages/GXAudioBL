88bd838d34a9091dce5f2c46bc93d813461d3925
Build Er <builder@macmini6.local>
2023-03-06 07:58:15 -0300

    Package version: 1.0.0-beta+20230306105223

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXAudioBL",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXAudioBL-1.0.0-beta+20230303092649.xcframework.zip",
-			checksum: "1f266e2d92c7dda3ce1e4efdd4e8ded8256dec0092966f0301a55bcd47c19a83"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXAudioBL-1.0.0-beta+20230306105223.xcframework.zip",
+			checksum: "0e53de2443d7d1080080063533319d9091d9bd269b59aeef879117942191df13"
 		)
 	]
 )
