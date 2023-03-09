ff016f044a0c63c4cdfdcdbb3aeb9955e88bb35e
genexusbot <builder@genexus.com>
2023-03-09 11:26:45 -0300

    Package version: 1.0.0-beta+20230309142042

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXAudioBL",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXAudioBL-1.0.0-beta+20230309134517.xcframework.zip",
-			checksum: "9d0af2556804c398d50d43014fa164f9699f4ea87cf3ea90ceb57313d8ee8c44"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXAudioBL-1.0.0-beta+20230309142042.xcframework.zip",
+			checksum: "34b18c5d4510c0897b839fb7a695b82c938fae95287028eb2c3ad7e820d5f89c"
 		)
 	]
 )
