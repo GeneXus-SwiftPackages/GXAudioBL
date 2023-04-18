9d823365557a4a02c823b587a766ac018bdbbfc8
Build Er <builder@macmini6.local>
2023-04-18 10:13:49 -0300

    Package version: 1.0.0-beta+20230418130717

--- Package.swift
+++ Package.swift
@@ -16,13 +16,13 @@ let package = Package(
 		.target(name: "GXAudioBLWrapper",
 				dependencies: [
 					"GXAudioBL",
-					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.iOS, .watchOS, .tvOS]))
+					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.iOS, .tvOS, .watchOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXAudioBL",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXAudioBL-1.0.0-beta+20230414101841.xcframework.zip",
-			checksum: "3d5618090b04dfb802067412421faeda3e221a21be68b6d121b875fdc0b37598"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXAudioBL-1.0.0-beta+20230418130717.xcframework.zip",
+			checksum: "959fe91dade0e485b46bc2efc76368763931e89c5c85f976296c3ecd5affe474"
 		)
 	]
 )
