6cc5993c02836b3017ac987b3b734d6a537458e2
genexusbot <builder@genexus.com>
2023-08-15 10:23:23 -0300

    Package version: 1.1.0

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+	name: "GXAudioBL",
+	platforms: [.iOS("12.0"), .watchOS("5.0"), .tvOS("12.0")],
+	products: [
+		.library(
+			name: "GXAudioBL",
+			targets: ["GXAudioBLWrapper"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "1.1.0")
+	],
+	targets: [
+		.target(name: "GXAudioBLWrapper",
+				dependencies: [
+					"GXAudioBL",
+					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.iOS, .tvOS, .watchOS]))
+				],
+				path: "Sources"),
+		.binaryTarget(
+			name: "GXAudioBL",
+			url: "https://pkgs.genexus.dev/iOS/releases/GXAudioBL-1.1.0.xcframework.zip",
+			checksum: "a6fe98282c6ef22c55006cce4b821321394ed93db57c769068c859c8a234ff8e"
+		)
+	]
+)
