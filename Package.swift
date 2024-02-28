efd9569ac5f4a2a573a08b9292c08bb6145cf45e
genexusbot <builder@genexus.com>
2024-02-28 09:12:28 -0300

    Package version: 1.5.0-beta.9

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["GXAudioBLWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "1.5.0-beta.8")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "1.5.0-beta.9")
 	],
 	targets: [
 		.target(name: "GXAudioBLWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXAudioBL",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXAudioBL-1.5.0-beta.8.xcframework.zip",
-			checksum: "254c36a35ecfbd94dd8edaf70372835255f644e0feea78ed9ffe215edac055c9"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXAudioBL-1.5.0-beta.9.xcframework.zip",
+			checksum: "3d1428662f697e35eca89edc78a94416e24c101a61b5fd41a3abfa86221876bd"
 		)
 	]
 )
